ab663857cf11be0a5f4206333b404259dd85cce6
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-21 19:53:39 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -9,16 +9,85 @@
 */
 
 #include <stdio.h>
+#define true 1
+#define false 0
+
+/**
+ * @brief fungsi eksekusi rekursi
+ * 
+ * @param prevVal elemen array pada indeks sebelum yang ditinjau
+ * @param addr alamat elemen yang sedang ditinjau
+ * @param idx indeks alamat yang sedang ditinjau
+ * @param last_idx indeks maksimum peninjauan
+ * @return int mengembalikan nilai hasil sekuens yang ditelusuri
+ */
+int execute(char prevVal, char* addr, int idx, int last_idx);
 
 int main() {
+	// Inisialisasi variabel string RNA
+	char RNA[30];
+	int last_idx = 0;
 
-	//template input
+	// Input string RNA
 	printf("Input RNA: ");
-	scanf("%s", ...);
+	scanf("%s", RNA);
 
-	// template output
-	printf("Pita RNA rusak");
-	printf("Panjang maximum: %d", ...);
-	
+	// Evaluasi validitas RNA
+	int i = 0; int exitLoop = false;
+	while (i < 30 && exitLoop == false) {
+		if ((RNA[i] == 'A') || (RNA[i] == 'U') || (RNA[i] == 'C') || (RNA[i] == 'G')) { // Kasus karakter sesuai karakter standar
+			if (i == 29) {                                                              // Jika karakter ditemukan pada indeks 29, maka batas sudah terlampaui
+				printf("Pita RNA rusak");
+				return 0;
+			}
+		}
+		else if (RNA[i] == 'X') {                                                       // Jika karakter X ditemukan, evaluasi apakah karakter selanjutnya merupakan akhir dari string
+			if (RNA[i+1] != '\0') {
+				printf("Pita RNA rusak");
+				return 0;
+			}
+			else {                                                                      // Jika setelah karakter X adalah end of string, maka loop diselesaikan
+				last_idx = i;
+				exitLoop = true;
+			}
+		}
+		else {                                                                          // Kondisi karakter selain karakter valid
+			printf("Pita RNA rusak");
+			return 0;
+		}
+		i++;
+	}
+
+	// Penghitungan jumlah sekuens terpanjang
+	int max = 0;
+	i = 1;
+	while (i < last_idx+1) {
+		int temp = execute(RNA[i-1], &(RNA[i]), i, last_idx);
+		if (temp > max) {
+			max = temp;
+		}
+		i = i + temp;
+	}
+
+	// Output sekuens terpanjang
+	printf("Panjang maximum: %d", max);
 	return 0;
+}
+
+int execute(char prevVal, char* addr, int idx, int last_idx) {
+	if (idx > last_idx) {
+		// Jika indeks sudah mencapai indeks maksimal, nilai 0 dikeluarkan
+		return 0;
+	}
+	else {
+		// Kondisi indeks valid
+		if (prevVal == *addr) {
+			// Kondisi konten string masih sama
+			return (1+execute(*addr, addr+1, idx+1, last_idx));
+		}
+		else {
+			// Kondisi konten string berbeda
+			return 1;
+		}
+	}
 }
